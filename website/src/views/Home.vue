4c6cb23553ec07dc2a5ad62296819b94c3d393c2
Herover <leon.l.a.nielsen@gmail.com>
2020-06-13 13:44:21 +0200

    Remove content from home page and add host input

--- website/src/views/Home.vue
+++ website/src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Enter IP address of server</h1>
+    <HostInput/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import HostInput from '@/components/HostInput.vue';
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld,
+    HostInput,
   },
 };
 </script>
